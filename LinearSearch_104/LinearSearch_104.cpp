a3be98ee396eb611e2ab32d7e8ec44d735d16ec9
alfinur154 <alfinur27@gmail.com>
2023-04-13 14:45:31 +0700

    memasukkan element array

--- LinearSearch_104/LinearSearch_104.cpp
+++ LinearSearch_104/LinearSearch_104.cpp
@@ -17,4 +17,13 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	// Accept array elements
+	cout << "\n-----------------\n";
+	cout << " Enter array elements \n";
+	cout << "-------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" <<
+	}
 }
